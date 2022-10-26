9bae0f003b95ae3dfd261dfd96076a1865ffff87
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-10-26 23:16:29 +0200

    Server_jar struct + Version enum

--- src/bfr.v
+++ src/bfr.v
@@ -5,9 +5,14 @@ const (
 	hash_1_19_1 = 'f69c284232d7c7580bd89a5a4931c3581eae1378'
 )
 
-struct Version {
-	number string
-	hash   string
+struct Server_jar {
+	version string
+	hash    string
+}
+
+enum Version {
+	1_19_1
+	1_19
 }
 
 fn main() {
