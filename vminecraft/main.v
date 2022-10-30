481c8ca3a55116da1e094480d2c1250efc89df75
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-10-30 02:18:34 +0200

    Added logger and checking file size

--- vminecraft/main.v
+++ vminecraft/main.v
@@ -1,23 +1,22 @@
-import net.http { download_file }
-import os { getwd }
-import bfrvlog
+import net.http { CommonHeader, get }
+import log { Level, Log, LogTarget }
 
 const (
 	download_url = 'https://piston-data.mojang.com/v1/objects/$hash_1_19_1/server.jar'
 	hash_1_19_1  = 'f69c284232d7c7580bd89a5a4931c3581eae1378'
 )
 
-struct Server_jar {
-	version string
-	hash    string
-}
-
 fn main() {
-	println('Currently downloading minecraft server version ...')
-	bfrvlog.hello()
-	download_file(download_url, getwd() + '\\server-minecraft.jar') or {
-		println(err)
+	mut logger := Log{
+		level: Level.debug
+		output_target: LogTarget.console
+	}
+	logger.info('Checking the size of the jar file...')
+	logger.error('Testing error with this instruction :(')
+	response := get(download_url) or {
+		logger.error(err.msg())
 		return
 	}
-	println('Download finished successfully !')
+	file_size := response.header.get(CommonHeader.content_length)!
+	logger.debug('the size of the file is currently $file_size octets')
 }
